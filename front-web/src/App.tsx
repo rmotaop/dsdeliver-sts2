2617067813a69aaef93e1631ba02646a3cade09a
rmotaop op <rmotaop@gmail.com>
2021-01-06 23:26:08 -0300

    Add routes and StepHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,13 @@
 
 import './App.css';
-import Navbar from './Navbar';
-import Home from './Home';
+
+import Routes from './Routes';
 //import Counter from './Counter';
 
 function App() {
   return (
     <>
-      <Navbar /> 
-      <Home />
+       <Routes />
     </>
   );
 }
