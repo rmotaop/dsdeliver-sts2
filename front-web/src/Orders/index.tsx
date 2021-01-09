c0aa2c03d8481cfe34603db5e78e6515095688c7
rmotaop op <rmotaop@gmail.com>
2021-01-08 21:54:28 -0300

    Search order location

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,12 +1,28 @@
-
-import StepsHeader from './StepsHeader';
 import './styles.css';
+import ProductsList from './ProductsList';
+import StepsHeader from './StepsHeader';
+import { useEffect, useState } from 'react';
+import { OrderLocationdata, Product } from './types';
+import { fecthProducts } from '../api';
+import OrderLocation from './OrderLocation';
+import { SelectorMatcherOptions } from '@testing-library/react';
+
 
 function Orders() {
-    
+
+    const [products, setProducts] = useState<Product[]>([]);
+    const [orderlocation, setOrderlocation] = useState<OrderLocationdata>();
+    useEffect(() => {
+            fecthProducts()
+                .then(response => setProducts(response.data))
+                .catch(error => console.log(error))
+    }, []);
+
     return(
         <div className="orders-container">
-            <StepsHeader/>
+            <StepsHeader />
+            <ProductsList products={products}/>
+            <OrderLocation onChangeLocation={location => setOrderlocation(location)}/>
         </div>
 )
 
---
+++ front-web/src/Orders/types.ts
@@ -0,0 +1,17 @@
+export type Product = {
+
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    imageUri: string;
+
+}
+
+export type orderLocationdata = {
+
+    latitude: number;
+    longitude: number;
+    address: string;
+
+}
